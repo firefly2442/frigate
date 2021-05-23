410af5d79f2bfcf3b62ceaf5d8414ff84f6badf2
Blake Blackshear <blakeb@blakeshome.com>
2021-05-23 08:38:31 -0500

    development tweaks

--- web/snowpack.config.js
+++ web/snowpack.config.js
@@ -3,6 +3,7 @@ module.exports = {
     public: { url: '/', static: true },
     src: { url: '/dist' },
   },
+  exclude: ['**/jsmpeg.*'],
   plugins: ['@snowpack/plugin-postcss', '@prefresh/snowpack', 'snowpack-plugin-hash'],
   routes: [{ match: 'routes', src: '.*', dest: '/index.html' }],
   optimize: {
